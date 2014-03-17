c06ffc7e4884b5d0a7e4589f7f80aae8fd99bf96
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2014-03-17 21:30:29 +0100

    dashboard page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Oceny::Application.routes.draw do
+  root 'dashboard#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
