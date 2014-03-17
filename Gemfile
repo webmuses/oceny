8c17b9467d0cbbe5dff9ca68f7b4b8ca9df41557
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2014-03-17 08:30:45 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+ruby '2.1.1'
+
+gem 'rails', '4.0.4'
+gem 'sass-rails', '~> 4.0.2'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'jbuilder', '~> 1.2'
+gem 'mongoid', github: 'mongoid/mongoid'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Oceny::Application.config.secret_key_base = '29bb1f91ad13363cfdb98913e99cbc72e6e168c9694cd9fadf031d38844260a5307d9307877d88196915eb5028957be56ff01e0dedf4fbc60514b157e96c5fa6'
