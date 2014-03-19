dffeb4a66c999cf7971194ed053140263f80453c
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2014-03-19 22:44:10 +0100

    next, prev attendee for current

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -3,4 +3,12 @@ class Attendee
   include Mongoid::Attributes::Dynamic
 
   field :_id, type: Integer
+
+  def next
+    self.class.where(:id.gt => id).order(:id.asc).first
+  end
+
+  def prev
+    self.class.where(:id.lt => id).order(:id.desc).first
+  end
 end
