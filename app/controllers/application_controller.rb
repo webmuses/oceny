93a760656f7bf0608f7f18f019b82f95683417e6
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2014-03-20 08:33:29 +0100

    authenticate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
+  before_action :authenticate
+
+  def authenticate
+    redirect_to(login_path) unless current_user
+  end
+
   def current_user
     begin
       @current_user ||= User.find(session[:user_id])
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate
+
   def create
     user = User.find_or_create_by(basic_user_attributes)
 
