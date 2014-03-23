f470c4b64c39d200dc49751865dbd404d772b6a2
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2014-03-23 12:04:49 +0100

    optimized query for attendees with rates

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,6 +1,6 @@
 class ResultsController < ApplicationController
   def index
-    attendees = AttendeeDecorator.decorate_collection(Attendee.all)
+    attendees = AttendeeDecorator.decorate_collection(Attendee.includes(:rates).all)
     results = AttendeeSelector.new(attendees).select!
 
     render json: results, each_serializer: AttendeeSingleSerializer
