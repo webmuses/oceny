c5cc0ce6e770a83cc5417b3c9d78089aeb633f65
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2015-04-01 13:29:00 +0200

    send emails with Mandrill

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Oceny::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.mandrillapp.com',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['MANDRILL_USERNAME'],
+  :password       => ENV['MANDRILL_APIKEY'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,4 +23,6 @@ Oceny::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
