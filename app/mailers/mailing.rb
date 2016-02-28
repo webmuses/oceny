78355255376570d55fb03f6714a10de71be8e058
Basia Madej <basia@lunarlogic.io>
2016-02-28 12:22:01 +0100

    cleanup old data

--- app/mailers/mailing.rb
+++ app/mailers/mailing.rb
@@ -2,7 +2,6 @@ class Mailing < ActionMailer::Base
   default from: 'railsgirlskrakow@gmail.com'
 
   def after_submission(submission)
-    email = submission.email
-    mail(to: email, subject: 'Rails Girls Kraków 2015: Thank you for your submission')
+    mail(to: submission.email, subject: 'Rails Girls Kraków 2016: Thank you for your submission')
   end
 end
